c3dac4282861cf4d4d97e9bd55d3fc54d3a194b2
Jeanette Rojas <jrojas@aclanyc.com>
2017-05-04 01:34:54 +0000

    i made a thing

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+var students = ["Jeanette", "Julio", "Brittany", "Wilder", "Ashley"];
+var student_chosen = students[Math.floor(Math.random()*students.length)];
 
+$("#studentButton").click(function() {
+  $("#studentDisplay").html(student_chosen + " " + "has been selected");
+});
+
+var teachers = ["Julia", "Zach", "Alyxe", "Justin", "Aaron"];
+var teacher_chosen = teachers[Math.floor(Math.random()*teachers.length)];
+
+$("#teacherButton").click(function() {
+  $("#teacherDisplay").html(teacher_chosen + " " + "has been selected");
+});
 
 });
